92e9e3af5a954be83312e0859baccdf85766d2fe
serAnkii <recruitankitnegi2023@gmail.com>
2022-07-26 16:38:01 +0530

    feat:some changes to ui

--- src/components/Adds.jsx
+++ src/components/Adds.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import { Input } from '@web3uikit/core'
+import "./css/ads.css"
 const Adds = () => {
   return (
     <>
     <div id="adds">
-      <iframe src="https://bio.link/serAnkii" frameborder="12">Reach out the developer</iframe>
-    </div>
+    <Input
+  hasCopyButton
+  label="Search Twitter"
+  name="search"
+  onBlur={function noRefCheck(){}}
+  onChange={function noRefCheck(){}}
+  type="text"
+/>
+</div>
+
+<div id='developer'>
+      <iframe src="https://bio.link/serAnkii" frameborder="12" height={300}>Reach out the developer</iframe>
+      </div>
     </>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,17 +7,28 @@ import {IoMdNotificationsOutline} from "react-icons/io"
 import {BiMessageSquareDetail} from "react-icons/bi"
 import {BsBookmarks} from "react-icons/bs"
 import {CgProfile} from "react-icons/cg"
+import {GiBirdTwitter} from "react-icons/gi"
+
+
+
 
 const Nav = () => {
   return (
     <>
+    <div id='icon_twit'>
+    <h1><GiBirdTwitter/>   </h1>
+    <h2>Sasta Twitter</h2>
+    </div>
     <div id='nav'>
     <Link className='link' to="/"><RiHomeSmileFill/>   Home</Link>
     <Link className='link' to="/Explore"><MdOutlineExplore/>   Explore</Link>
     <Link className='link' to="/Notifications"><IoMdNotificationsOutline/>   Notifications</Link>
     <Link className='link' to="/Messages"><BiMessageSquareDetail/>   Messages</Link>
     <Link className='link' to="/Bookmarks"><BsBookmarks/>   Bookmarks</Link>
-    <Link className='link' to="/Profile"><CgProfile/>   Profile</Link>
+    <Link className='link' to="/Profile"><CgProfile/>  Profile</Link>
+    </div>
+    <div id='tweet'>
+    <button>Tweet</button>
     </div>
     </>
   )
