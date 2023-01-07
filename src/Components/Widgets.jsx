5daad63b2159ef07eb2017009518842481cf5e93
keshav660 <keshavbhatt660@gmail.com>
2023-01-07 14:31:39 +0530

    feat:widget-keshav

--- src/Components/Widgets.jsx
+++ src/Components/Widgets.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Widgets = () => {
+import "../css/Widgets.css"
+
+function Widgets(){
+
+  
   return (
-    <div>Widgets</div>
+    <div id='widget'>
+        <h1>this is widget 1</h1>
+        <h1>this is widget 2 </h1>
+        <h1>this is widget 3</h1>
+        <h1>this is widget 4</h1>
+        <h1>this is widget 5</h1>
+    </div>
   )
 }
 
