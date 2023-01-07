f0cbb9b1e9c1175cb6d7091c6f4c772254cf0fba
serAnkii <recruitankitnegi2023@gmail.com>
2023-01-07 08:32:24 +0530

    feat:reincarnation of repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import "./app.css"
-import {Nav} from "./components/Nav"
-import Content from "./components/content/Content"
-import Adds from "./components/Adds"
 export default function App(){
 return (
   <>
-  <div id='main_div'>
-    <div id='section1' className='section' ><Nav /></div>
-    <div id="section2"  className='section'><Content/></div>
-    <div id="section3"  className='section'><Adds/></div>
-  </div>
+  <div>Twitter sperm</div>
   </>
 )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <MoralisProvider appId={""} serverUrl={""}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </MoralisProvider>
 );
