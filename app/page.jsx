de41a003ef5fb99dabe18bcd3f7064f8bede819e
serAnkii <recruitankitnegi2023@gmail.com>
2023-05-15 18:44:35 +0530

    feat:init

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to twitter clone ! We are migrating from react to next </h1>
+    </>
+  )
+}
