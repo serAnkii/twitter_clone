1df681e55f77c3a17657a9619c2110a193a4e4bf
serAnkii <recruitankitnegi2023@gmail.com>
2023-05-16 21:48:12 +0530

    feat:some changes

--- app/compnents/TweetBox.jsx
+++ app/compnents/TweetBox.jsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 const TweetBox = () => {
   return (
     <div className={styles.tweetbox}>
-      <Image height={10} width={10}></Image>
+      <Image height={50} width={50} style={{border:"1px solid white",borderRadius:"50%"}}></Image>
       <form action="" method="post" className={styles.tweetboxform}>
-        <input type="text" placeholder="What's happening?" className={styles.tweetboxforminput}/>
+        <input type="text" placeholder="What's happening?!" className={styles.tweetboxforminput}/>
         <div className={styles.attachments}>
-            <Image width={5} height={5} alt="icon" className={styles.input_icons} />
-            <Image width={5} height={5} alt="icon" className={styles.input_icons} />
-            <Image width={5} height={5} alt="icon" className={styles.input_icons} />
-            <Image width={5} height={5} alt="icon" className={styles.input_icons} />
-            <Image width={5} height={5} alt="icon" className={styles.input_icons} />
-            <Image width={5} height={5} alt="icon" className={styles.input_icons} />
-            <button>Tweet</button>
+            <Image width={5} height={5} alt="icon" className={`${styles.input_icons} ${styles.input_items}`}  />
+            <Image width={5} height={5} alt="icon" className={`${styles.input_icons} ${styles.input_items}`}  />
+            <Image width={5} height={5} alt="icon" className={`${styles.input_icons} ${styles.input_items}`}  />
+            <Image width={5} height={5} alt="icon" className={`${styles.input_icons} ${styles.input_items}`}  />
+            <Image width={5} height={5} alt="icon" className={`${styles.input_icons} ${styles.input_items}`}  />
+            <Image width={5} height={5} alt="icon" className={`${styles.input_icons} ${styles.input_items}`}  />
+            <button className={`${styles.input_items} ${styles.tweet_button}`}>Tweet</button>
         </div>
       </form>
     </div>
