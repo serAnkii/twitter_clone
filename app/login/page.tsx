094c0130f22a81c02d285094bd25860ae881f13f
serAnkii <recruitankitnegi2023@gmail.com>
2023-10-07 22:03:04 +0530

    feat:some ui changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,13 @@
 import "@/app/globals.css";
-
-export default  function Login() {
+import Link from "next/link";
+export default function Login() {
   return (
     <>
       <div
         id="mainloginpage"
         className="bg-black h-screen w-screen flex  flex-col absolute left-0 z-50"
       >
-        <h1 className="text-[5vw] w-screen text-center font-bold text-twitter">
+        <h1 className="text-[5rem] w-screen text-center font-bold text-twitter">
           Twitter <span className="text-pink-600"> X</span>
         </h1>
 
@@ -15,13 +15,18 @@ export default  function Login() {
           <div className="flex w-screen h-[100%] ">
             <div
               id="sign-in"
-              className="w-[50%] p-4 h-[100%] flex flex-col gap-5 "
+              className=" backdrop-blur-sm w-[100%] p-4 h-[100%] flex flex-col gap-5 "
             >
               <h1 className="text-4xl text-center tracking-[0.5rem] w-100%">
                 LOGIN
               </h1>
 
-              <form id="form-signin" className="items-center justify-center" action="/auth/sign-in" method="post">
+              <form
+                id="form-signin"
+                className="items-center justify-center"
+                action="/auth/sign-in"
+                method="post"
+              >
                 <input
                   name="email"
                   className="text-slate-950 rounded-xl text-center"
@@ -36,46 +41,17 @@ export default  function Login() {
                 />
 
                 <div className="flex justify-center w-full items-center">
-                <button className=" bg-twitter text-3xl font-bold p-4 rounded-full hover:scale-105 transition-all duration-200">
-                  Sign-in
-                </button>
-                </div>
-              </form>
-            </div>
-            <div id="sign-up" className="w-[50%] p-4 h-[100%]  flex flex-col">
-              <h1 className="text-4xl tracking-[0.5rem] text-center w-[100%]">
-                SIGN UP
-              </h1>
-
-              <form
-                id="from_signup"
-                action="/auth/sign-up"
-                method="post"
-                className=" justify-center items-center"
-              >
-                <input
-                  name="email"
-                  className="text-slate-950 rounded-xl text-center"
-                  placeholder="Enter a vaid email"
-                  required
-                />
-                <input
-                  className="text-slate-950 rounded-xl text-center"
-                  placeholder="Enter a strong Password"
-                  type="password"
-                  name="password"
-                />
-
-                <input
-                  className="text-slate-950 rounded-xl text-center"
-                  placeholder="Enter your username"
-                  type="text"
-                  name="uname"
-                />
-                <div className="flex justify-center w-full items-center ">
-                  <button className=" bg-twitter text-3xl font-bold rounded-full hover:scale-105 p-4 transition-all duration-200">
-                    Sign-up
+                  <button className=" bg-twitter text-3xl font-bold p-4 rounded-full hover:scale-105 transition-all duration-200">
+                    Sign-in
                   </button>
+                </div>
+                <div className="flex justify-center items-center">
+                <Link
+                  href={"/signup"}
+                  className="font-bold"
+                >
+                  New User?Sign-up!
+                </Link>
                 </div>
               </form>
             </div>
---
+++ app/signup/page.tsx
@@ -0,0 +1,66 @@
+import "@/app/globals.css";
+import Link from "next/link";
+export default function Login() {
+  return (
+    <>
+      <div
+        id="mainloginpage"
+        className="bg-black h-screen w-screen flex  flex-col absolute left-0 z-50"
+      >
+        <h1 className="text-[5rem] w-screen text-center font-bold text-twitter">
+          Twitter <span className="text-pink-600"> X</span>
+        </h1>
+
+        <div className="flex w-screen h-[70%]">
+          <div className="flex w-screen h-[100%] ">
+            <div id="sign-up" className="backdrop-blur-sm w-[100%] p-4 h-[100%]  flex flex-col">
+              <h1 className="text-4xl tracking-[0.5rem] text-center w-[100%]">
+                SIGN UP
+              </h1>
+
+              <form
+                id="from_signup"
+                action="/auth/sign-up"
+                method="post"
+                className=" justify-center items-center"
+              >
+                <input
+                  name="email"
+                  className="text-slate-950 rounded-xl text-center"
+                  placeholder="Enter a vaid email"
+                  required
+                />
+                <input
+                  className="text-slate-950 rounded-xl text-center"
+                  placeholder="Enter a strong Password"
+                  type="password"
+                  name="password"
+                />
+
+                <input
+                  className="text-slate-950 rounded-xl text-center"
+                  placeholder="Enter your username"
+                  type="text"
+                  name="uname"
+                />
+                <div className="flex justify-center w-full items-center ">
+                  <button className=" bg-twitter text-3xl font-bold rounded-full hover:scale-105 p-4 transition-all duration-200">
+                    Sign-up
+                  </button>
+                </div>
+                <div className="flex justify-center items-center">
+                <Link
+                  href={"/login"}
+                  className="font-bold"
+                >
+                  Sign-in?
+                </Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
